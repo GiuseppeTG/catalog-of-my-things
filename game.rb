7cbd4d8950e2d3b865e597d2c967e093712cc6ca
waqas ahmad <waqaskanju@gmail.com>
2022-09-06 22:37:47 +0500

    add game class

---
+++ game.rb
@@ -0,0 +1,20 @@
+require_relative './item.rb'
+
+class Game < Item
+
+    def initialize(multiplayer,last_played_at)
+        @multiplayer = multiplayer
+        @last_played_at = last_played_at
+    end
+
+    private
+
+    def can_be_archived?()
+        value = false
+        if super(can_be_archived?) || last_played_at
+            value =true
+        end
+        value
+    end
+
+end
