4cb2e7b907b6d000b28c48989e95ebfeb5792569
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-07 08:47:52 +0100

    Add constructor method to Source class

--- source.rb
+++ source.rb
@@ -1,4 +1,10 @@
 require_relative './item'
 
 class Source < item
+  def initialize(title, publish_date, name id = nil)
+    super(title, publish_date, id)
+
+    @id = id || Random.rand(1..1_000_000)
+    @items = []
+  end
 end
