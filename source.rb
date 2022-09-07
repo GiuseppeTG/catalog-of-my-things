80ed746e0724ff592d49fcda82d66bcd1bcba1a5
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-07 18:50:15 +0100

    Add name as Source constructor parameter

--- source.rb
+++ source.rb
@@ -2,11 +2,12 @@ require_relative './item'
 
 class Source < item
   attr_reader :items
-  def initialize(title, publish_date, name id = nil)
+  def initialize(title, publish_date, name, id = nil)
     super(title, publish_date, id)
 
     @id = id || Random.rand(1..1_000_000)
     @items = []
+    @name = name
   end
 
   def add_item(item)
