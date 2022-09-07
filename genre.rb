262b3e4f92156b193165d9b94e98781918ca8f33
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-09-07 19:20:02 -0300

    Add initial work for genre.class

---
+++ genre.rb
@@ -0,0 +1,31 @@
+class Genre
+  attr_accessor :name
+  attr_reader :id, :items
+
+  def initialize(name)
+    @id = rand(1..1_000_000)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    # return unless item.instance_of?(Item) && !@items.include?(item)
+
+    @items.push(item)
+    item.add_genre(self)
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => @id,
+      'name' => @name,
+      'items' => @items
+    }.to_json(*args)
+  end
+
+  ### review below line (for better understand as it was '_book')
+  def self.json_create(_genre)
+    new(genre['id'], genre['name'], genre['items'])
+  end
+end
