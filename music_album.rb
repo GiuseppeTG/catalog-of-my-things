9827af9c2c1897e61522759ca921abe6a32b9535
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-09-08 14:44:10 -0300

    Update music_album.rb file

--- music_album.rb
+++ music_album.rb
@@ -8,8 +8,8 @@ class MusicAlbum < Item
 
   def initialize(*args, publisher, on_spotify)
     super(*args)
-    @on_spotify = on_spotify
     @publisher = publisher
+    @on_spotify = on_spotify
   end
 
   def can_be_archived?
@@ -23,27 +23,18 @@ class MusicAlbum < Item
       'publish_date' => @publish_date,
       'archived' => @archived,
       'title' => @title,
-      'on_spotify' => @on_spotify,
+      'cover_state' => @cover_state,
       'publisher' => @publisher,
-      'label_id' => @label.id,
-      'author_id' => @author,
-      'genre_id' => @genre,
-      'source_id' => @source
+      'label' => @label ? ['id' => @label.id, 'title' => @label.title, 'color' => @label.color] : nil,
+      'author' => @author ? ['id' => @author.id, 'first_name' => @author.first_name, 'last_name' => author.last_name] : nil,
+      'genre' => @genre ? ['id' => @genre.id, 'name' => @genre.name] : nil,
+      'source' => @source ? ['id' => @source.id, 'name' => @source.name] : nil
     }.to_json(*args)
   end
 
   def self.json_create(music_album)
-    new(music_album['id'], music_album['title'], music_album['publish_date'], music_album['archived'],
-        music_album['on_spotify'], music_album['publisher'], music_album['author'], music_album['label'],
-        music_album['genre'], music_album['source'])
+    new(music_album['id'], music_album['title'], music_album['publish_date'], music_album['on_spotify'],
+        music_album['publisher'], music_album['author'], music_album['label'], music_album['genre'],
+        music_album['source'])
   end
 end
-
-# b1 = Book.new('1984', '2000-08-23', 'publisher1', 'bad')
-# label = Label.new('NEW', 'yellow')
-# b1.add_label(label)
-# json = b1.to_json
-# p json
-# p b1.can_be_archived?
-# b1.move_to_archive
-# p b1
