321684a042578dd8ac04a41cbd94aba732235b6e
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-08 19:43:29 +0100

    Add test for Source class properties when item is added

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -11,4 +11,19 @@ describe Source do
       expect(source.items.empty?).to be true
     end
   end
+
+  context 'When an item is added' do
+    name = 'Wikipedia'
+    title = 'The Shawshank Redemption'
+    publish_date = '2009-10-10'
+
+    it 'number of items increments' do
+      source = Source.new(name)
+      item = Item.new(title, publish_date)
+      items_length = source.items.length
+      source.add_item(item)
+
+      expect(source.items.length).to be items_length + 1
+    end
+  end
 end
