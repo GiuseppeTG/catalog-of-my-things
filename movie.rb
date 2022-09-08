0f4f1ccb9ddd62da5c371eb9e39624b73b2daa3a
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-08 05:17:49 +0100

    Complete the Movie constructor

--- movie.rb
+++ movie.rb
@@ -1,7 +1,8 @@
 require_relative './item'
 
 class Movie < Item
-  def initialize
-    
+  def initialize(title, publish_date, silent=false)
+    super(title, publish_date, id = nil)
+    @silent = silent
   end
 end
