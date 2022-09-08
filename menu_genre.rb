cf04f31c6f4c5edee76fc5f643226042a9af96a9
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-09-08 14:33:09 -0300

    Update menu_genre.rb

--- menu_genre.rb
+++ menu_genre.rb
@@ -1,6 +1,6 @@
 class MenuGenre
   def genre_options(item, genres)
-    puts 'Do you want to add a genre to the music album? [Y/N]'
+    puts 'Do you want to add a genre to the book? [Y/N]'
     answer = gets.chomp.upcase
     return if %w[N NO].include?(answer)
 
@@ -12,10 +12,13 @@ class MenuGenre
 
     puts 'Enter a genre name'
     input_genre_name = gets.chomp.capitalize
-
-    genre = Genre.new(input_genre_name)
-
-    item.add_genre(genre)
-    genres << genre
+    genre = genres.find { |lab| lab['name'] == input_genre_name }
+    new_genre = if genre
+                  Genre.new(genre['name'], genre['id'])
+                else
+                  Genre.new(input_genre_name)
+                end
+    item.add_genre(new_genre)
+    genres << new_genre unless genre
   end
 end
